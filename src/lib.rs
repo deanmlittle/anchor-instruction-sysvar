937f89640e9b0e20bc51a7be6a5f2b154236bc7f
aresastro <dean@bitping.com>
2023-09-20 07:17:58 +0800

    Removed non-executable program

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 pub mod instructions;
 pub use instructions::*;
 
-pub mod non_executable_program;
-pub use non_executable_program::*;
-
-pub struct InstructionSysvar;
-
 // Need to figure out what type this should be. It's *like* a program, but not executable 🤔
 impl anchor_lang::Id for InstructionSysvar {
     fn id() -> solana_program::pubkey::Pubkey {
